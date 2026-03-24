c4fb945887da941fcb16941b4ee027fa47b1eec0
MinhNghia5 <524k0032@student.tdtu.edu.vn>
2026-03-24 22:33:34 +0700

    Added Student Name field

--- WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Tinnghia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(461, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 82);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Tinnghia
+            // 
+            this.Tinnghia.AutoSize = true;
+            this.Tinnghia.Location = new System.Drawing.Point(343, 214);
+            this.Tinnghia.Name = "Tinnghia";
+            this.Tinnghia.Size = new System.Drawing.Size(59, 16);
+            this.Tinnghia.TabIndex = 2;
+            this.Tinnghia.Text = "Tinnghia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Tinnghia);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Tinnghia;
     }
 }
 
